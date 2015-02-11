e6156b1297e721be435d87b96ea23f7492f5b5e0
tjones788 <tyler.jones@coyotes.usd.edu>
2015-02-11 15:37:27 -0600

    upload

--- BTL-HTML/public_html/js/TestJS.js
+++ BTL-HTML/public_html/js/TestJS.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("TEST!");
 
     $.ajax({
-        url: 'http://206.176.2.147:8080/BTL-REST/resources/tools',
+        url: 'http://localhost:8080/BTL-REST/resources/tools',
         type: 'GET',
         success: function(resp) {
             alert(resp);
