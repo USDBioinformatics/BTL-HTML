d4390752ae6d4b956da050e4b7689ae98f93e92a
csctar788 <tyler.jones@coyotes.usd.edu>
2015-02-13 06:05:18 -0600

    Added REST test Buttons

--- BTL-HTML/public_html/js/TestJS.js
+++ BTL-HTML/public_html/js/TestJS.js
@@ -1,15 +1,37 @@
+$(document).ready(function () {
 
-$.ajax({
-    url: 'http://localhost:8080/BTL-REST/resources/tools/names',
-    type: 'GET',
-    data: {
-        format: 'json'
-    },
-    success: function (resp) {
-        $("#notificationBar").addClass("successMessage").html("<h1>Success!</h1>");
-        $.each(resp, function (i, f) {
-            var tool_div_row = "<div class='toolRow'>" + "<h2>" + f + "</h2>" + "</div>";
-            $(tool_div_row).appendTo("#tools");
+    $('#allNames').on('click', function (e) {
+        callRest($(this).data("url"));
+    });
+
+    $('#allBets').on('click', function (e) {
+        callRest($(this).data("url"));
+    });
+
+    $('#allBridges').on('click', function (e) {
+        callRest($(this).data("url"));
+    });
+
+    function callRest(RestUrl, type) {
+        $.ajax({
+            url: RestUrl,
+            type: 'GET',
+            data: {
+                format: 'json'
+            },
+            success: function (resp) {
+                console.log("REST RESPONSE: " + resp);
+                $("#notificationBar").empty(); //clears tool data currently displayed
+                $("#tools").empty(); //clears tool data currently displayed
+                $("#notificationBar").addClass("successMessage").html("<h1>Success!</h1>");
+                
+                $.each(resp, function (i, f) {
+                    var tool_div_row = "<div class='toolRow'>" + "<h2>" + f + "</h2>" + "</div>";
+                    $(tool_div_row).appendTo("#tools");
+                });
+            }
         });
+    }
+    ;
 
-
+});
